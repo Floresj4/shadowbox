38e3dc1e0adae410773fc3f435b8489c4cb38c38
Jason <floresj4@gmail.com>
2018-03-03 12:05:01 -0500

    separating javascript

---
+++ shadowbox.js
@@ -0,0 +1,34 @@
+$(function(){
+    console.log('ready...');
+    $('.shadow-box').css('visibility', 'hidden');
+    $('.shadow-content').css('visibility', 'hidden');
+
+    $('#open-button').click(() => {
+
+        //get shadow-content dimensions
+        var height = $('.shadow-content').height();
+        var width = $('.shadow-content').height();
+
+        var docHeight = $(document).height();
+        var marginTop = (docHeight - height) / 2;
+        var docWidth = $(document).width();
+        var marginLeft = (docWidth - width) / 2;
+
+        $('.shadow-content').css({
+            'visibility': 'visible',
+            'position': 'absolute',
+            'top': marginTop + 'px',
+            'left': marginLeft + 'px',
+            'z-index': 10,
+        });
+
+        $('.shadow-box').css({
+            'visibility': 'visible',
+            'position': 'absolute',
+            'top': '0',
+            'left': '0',
+            'width': '100%',
+            'height': '100%'
+        });
+    });
+});
