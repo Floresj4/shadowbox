851a343fd224c8ed90cbf5068be9b8885dbc2b94
Jason <floresj4@gmail.com>
2018-03-13 21:51:06 -0400

    removing deprecated code and updating positionin logic

--- docs/shadowbox.js
+++ docs/shadowbox.js
@@ -3,7 +3,12 @@ var $shadowBox;
 var $shadowCloseBtn;
 
 var closeBtnHTML = "<button type='button' class='btn btn-danger btn-shadow-close'>Close</button>";
-var shadowBoxHtml = "<div class='shadow-box'></div>";
+var shadowOverlay = "<div class='shadow-overlay'></div>";
+
+var defaultcss = {
+    display: 'none',
+    position: 'absolute'
+};
 
 function getOrDefault(properties, propertyToFind, defaultValue) {
     return (properties !== undefined)
@@ -12,93 +17,29 @@ function getOrDefault(properties, propertyToFind, defaultValue) {
                 : defaultValue;
 }
 
-function collectShadowElements() {
-    let elements = [];
+function shadowbox(properties) {
+    //append shadowbox container
+    if(!exists('.shadow-overlay')) {
+        $('body').append(shadowOverlay);
+    }
+
     $("[shadowbox]").each((i, elem) => {
         let clazz = '.' + $(elem).attr('shadowbox');
 
-        $(elem).hide();
+        $(elem).css(defaultcss).hide();
         $(clazz).on('click', () => {
-            $(elem).toggle();
-        });
-    });
-
-    return elements;
-}
-function shadowbox(properties) {
-    //append shadowbox container
-    if(!exists('shadow-box')) {
-        $('body').append(shadowBoxHtml);
-    }
-
-    var shadowElements = collectShadowElements();
-
-    //initialize selectors
-    $shadowContent = $(properties.display);
-    $shadowBox = $('.shadow-box');
-    $shadowCloseBtn = $('.btn-shadow-close');
-
-    let overlayCss = overlay(properties);
-    $shadowBox.css(overlayCss);
-
-    //attach click event
-    $(properties.click).click(() => {
-
-        //make sure to not add duplicates
-        if($(properties.display + " > .btn-shadow-close").length == 0) {
-            $shadowContent.append(closeBtnHTML);
-        }
-
-        //get shadow-content dimensions
-        var zIndex = 10;
-        let css = Object.assign(
-            position(properties.display),
-            { 'z-index': zIndex }
-        );
-
-        $shadowContent.css(css).show();
-        $shadowBox.show();
-
-        //attach dynamic click event
-        $('body').on('click', properties.display + ' > .btn-shadow-close', () => {
-            $shadowBox.hide();
-            $shadowContent.hide();
-        });
-
-        $(window).resize(()=> {
-            $shadowContent.css(position(properties.display));
+            $(elem).css(position(elem)).toggle();
         });
     });
 }
 
 function exists(selector) {
-    return $(selector).length == 0;
+    return $(selector).length != 0;
 }
 
 function position(selector) {
-    return css = {
+    return {
         'top': ($(document).height() - $(selector).height()) / 2 + 'px',
         'left': ($(document).width() - $(selector).width()) / 2 + 'px'
-    }
-}
-
-function overlay(properties) {
-    let background = getOrDefault(properties, 'background', '#000');
-    let zIndex = getOrDefault(properties, 'z-index', 5);
-    let opacity = getOrDefault(properties, 'opacity', .7);
-    let msOpacity = (opacity * 100);
-
-    return {
-        '-ms-filter': 'progid:DXImageTransform.Microsoft.Alpha(Opacity=' + msOpacity +')',
-        filter: 'alpha(opacity=' + msOpacity +')',
-        'z-index': zIndex,
-        'background': background,
-        opacity: opacity,
-        display: 'none',
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%'
     };
 }
