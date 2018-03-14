78110fcb4fc534131738438575aa545e2a43ff84
Jason <floresj4@gmail.com>
2018-03-14 16:42:53 -0400

    refactoring to use shadowbox attribute

--- docs/shadowbox.js
+++ docs/shadowbox.js
@@ -1,15 +1,13 @@
-var $shadowContent;
-var $shadowBox;
-var $shadowCloseBtn;
 
 var closeBtnHTML = "<button type='button' class='btn btn-danger btn-shadow-close'>Close</button>";
 
+var overlaycss;
 var defaultcss = {
     display: 'none',
     position: 'absolute'
 };
 
-var overlaycss = {
+var defaultoverlaycss = {
     display: 'none',
     position: 'absolute',
     width: '100%',
@@ -29,6 +27,7 @@ function shadowbox(properties) {
 
     $('body').append("<div class='shadow-overlay'></div>");
     let shadow = $('.shadow-overlay');
+    overlaycss = getOverlaycss(properties);
     shadow.css(overlaycss);
 
     $("[shadowbox]").each((i, elem) => {
@@ -36,15 +35,31 @@ function shadowbox(properties) {
 
         $(elem).css(defaultcss).hide();
         $(clazz).on('click', () => {
-
             shadow.toggle();
 
-            $(elem).css(position(elem)).toggle();
+            let elemcss = Object.assign(getPosition(elem),
+                { 'z-index': overlaycss['z-index'] + 1 });
+            $(elem).css(elemcss)
+                .toggle();
         });
     });
 }
 
-function position(selector) {
+function getOverlaycss(properties) {
+    let background = getOrDefault(properties, 'background', '#000');
+    let opacity = getOrDefault(properties, 'opacity', '.7');
+    let zIndex = getOrDefault(properties, 'z-index', 5);
+    let msopacity = opacity * 100;
+    return Object.assign(defaultoverlaycss, {
+        'background': background,
+        '-ms-filter': 'progid:DXImageTransform.Microsoft.Alpha(Opacity=' + msopacity +')',
+        filter: 'alpha(opacity=' + msopacity +')',
+        'opacity': opacity,
+        'z-index': zIndex
+    });
+}
+
+function getPosition(selector) {
     return {
         'top': ($(document).height() - $(selector).height()) / 2 + 'px',
         'left': ($(document).width() - $(selector).width()) / 2 + 'px'
--- shadowbox.js
+++ shadowbox.js
@@ -1,9 +1,20 @@
-var $shadowContent;
-var $shadowBox;
-var $shadowCloseBtn;
 
 var closeBtnHTML = "<button type='button' class='btn btn-danger btn-shadow-close'>Close</button>";
-var shadowBoxHtml = "<div class='shadow-box'></div>";
+
+var overlaycss;
+var defaultcss = {
+    display: 'none',
+    position: 'absolute'
+};
+
+var defaultoverlaycss = {
+    display: 'none',
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    top: 0,
+    left: 0
+};
 
 function getOrDefault(properties, propertyToFind, defaultValue) {
     return (properties !== undefined)
@@ -13,79 +24,44 @@ function getOrDefault(properties, propertyToFind, defaultValue) {
 }
 
 function shadowbox(properties) {
-    //append shadowbox container
-    if(!exists('shadow-box')) {
-        $('body').append(shadowBoxHtml);
-    }
 
-    console.log('shadowbox components: ' + $("div[shadowbox='open-me']"));
+    $('body').append("<div class='shadow-overlay'></div>");
+    let shadow = $('.shadow-overlay');
+    overlaycss = getOverlaycss(properties);
+    shadow.css(overlaycss);
 
-    //initialize selectors
-    $shadowContent = $(properties.display);
-    $shadowBox = $('.shadow-box');
-    $shadowCloseBtn = $('.btn-shadow-close');
+    $("[shadowbox]").each((i, elem) => {
+        let clazz = '.' + $(elem).attr('shadowbox');
 
-    let overlayCss = overlay(properties);
-    $shadowBox.css(overlayCss);
+        $(elem).css(defaultcss).hide();
+        $(clazz).on('click', () => {
+            shadow.toggle();
 
-    //attach click event
-    $(properties.click).click(() => {
-
-        //make sure to not add duplicates
-        if($(properties.display + " > .btn-shadow-close").length == 0) {
-            $shadowContent.append(closeBtnHTML);
-        }
-
-        //get shadow-content dimensions
-        var zIndex = 10;
-        let css = Object.assign(
-            position(properties.display),
-            { 'z-index': zIndex }
-        );
-
-        $shadowContent.css(css).show();
-        $shadowBox.show();
-
-        //attach dynamic click event
-        $('body').on('click', properties.display + ' > .btn-shadow-close', () => {
-            $shadowBox.hide();
-            $shadowContent.hide();
-        });
-
-        $(window).resize(()=> {
-            $shadowContent.css(position(properties.display));
+            let elemcss = Object.assign(getPosition(elem),
+                { 'z-index': overlaycss['z-index'] + 1 });
+            $(elem).css(elemcss)
+                .toggle();
         });
     });
 }
 
-function exists(selector) {
-    return $(selector).length == 0;
+function getOverlaycss(properties) {
+    let background = getOrDefault(properties, 'background', '#000');
+    let opacity = getOrDefault(properties, 'opacity', '.7');
+    let zIndex = getOrDefault(properties, 'z-index', 5);
+    let msopacity = opacity * 100;
+    return Object.assign(defaultoverlaycss, {
+        'background': background,
+        '-ms-filter': 'progid:DXImageTransform.Microsoft.Alpha(Opacity=' + msopacity +')',
+        filter: 'alpha(opacity=' + msopacity +')',
+        'opacity': opacity,
+        'z-index': zIndex
+    });
 }
 
-function position(selector) {
-    return css = {
+function getPosition(selector) {
+    return {
         'top': ($(document).height() - $(selector).height()) / 2 + 'px',
         'left': ($(document).width() - $(selector).width()) / 2 + 'px'
-    }
-}
-
-function overlay(properties) {
-    let background = getOrDefault(properties, 'background', '#000');
-    let zIndex = getOrDefault(properties, 'z-index', 5);
-    let opacity = getOrDefault(properties, 'opacity', .7);
-    let msOpacity = (opacity * 100);
-
-    return {
-        '-ms-filter': 'progid:DXImageTransform.Microsoft.Alpha(Opacity=' + msOpacity +')',
-        filter: 'alpha(opacity=' + msOpacity +')',
-        'z-index': zIndex,
-        'background': background,
-        opacity: opacity,
-        display: 'none',
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%'
     };
 }
